c5ea8a58ea42ebfe3beeb41537d3516d53a47391
okaformark <okaformark@gmail.com>
2019-11-24 12:09:16 -0600

    set up react frontend

---
+++ FlowersAndFloofs.ui/flowersandfloofs/src/Components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <button className="btn btn-danger">help</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
