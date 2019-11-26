21f4e5472349bde9b2a39f6c28f62bb08fe5a8d8
Matt Gill <mtgill90@outlook.com>
2019-11-25 21:17:09 -0600

    wip

---
+++ FlowersAndFloofs/Controllers/PaymentMethodController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlowersAndFloofs.DataAccess;
+using FlowersAndFloofs.DTOs;
+using FlowersAndFloofs.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowersAndFloofs.Controllers
+{
+    [Route("api/paymentMethod")]
+    [ApiController]
+    public class PaymentMethodController : ControllerBase
+    {
+        // GET: api/PaymentMethod/5
+        [HttpGet("{id}")]
+        public IEnumerable<PaymentMethod> GetByCustomerId(int id)
+        {
+            var repo = new PaymentMethodRepository();
+            var paymentMethodsById = repo.GetPaymentMethodByCustomerId(id);
+            return paymentMethodsById;
+        }
+
+        // POST: api/PaymentMethod
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/PaymentMethod/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FlowersAndFloofs/DataAccess/PaymentMethodRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using FlowersAndFloofs.Models;
+using System.Data.SqlClient;
+
+namespace FlowersAndFloofs.DataAccess
+{
+    public class PaymentMethodRepository
+    {
+        string _connectionString = "Server=localhost;Database=FlowersAndFloofs;Trusted_Connection=True;";
+
+        public IEnumerable<PaymentMethod> GetPaymentMethodByCustomerId(int customerId)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql = @"Select *
+                            from PaymentMethod
+                            where CustomerId = @customerId";
+                var parameters = new
+                {
+                    CustomerId = customerId
+                };
+                var customerPaymentMethods = db.Query<PaymentMethod>(sql, parameters);
+                return customerPaymentMethods;
+            }
+        }
+    }
+}
