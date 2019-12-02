b084e36eb3be640a5eb190045a0d2ce04c53391d
Matt Gill <mtgill90@outlook.com>
2019-12-02 16:11:20 -0600

    wip - get all occasions works

---
+++ FlowersAndFloofs/Controllers/OccasionController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlowersAndFloofs.DataAccess;
+using FlowersAndFloofs.DTOs;
+using FlowersAndFloofs.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowersAndFloofs.Controllers
+{
+    [Route("api/occasion")]
+    [ApiController]
+    public class OccasionController : ControllerBase
+    {
+        // GET: api/Occasion
+        [HttpGet]
+        public IEnumerable<Occasion> Get()
+        {
+            var repo = new OccasionRepository();
+            var allOccasions = repo.GetAllOccasions();
+            return allOccasions;
+        }
+
+        // POST: api/Occasion
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Occasion/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FlowersAndFloofs/DataAccess/OccasionRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using FlowersAndFloofs.Models;
+using System.Data.SqlClient;
+using FlowersAndFloofs.DTOs;
+
+
+namespace FlowersAndFloofs.DataAccess
+{
+    public class OccasionRepository
+    {
+        string _connectionString = "Server=localhost;Database=FlowersAndFloofs;Trusted_Connection=True;";
+
+        public IEnumerable<Occasion> GetAllOccasions()
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var sql = @"Select *
+                            from Occasion";
+
+                var allOccasions = db.Query<Occasion>(sql);
+                return allOccasions;
+            }
+        }
+    }
+}
