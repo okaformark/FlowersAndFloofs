6da474297bd9687670a7e11f5dc0e2753c1ed6ef
EmilyDeWitt <emilykdewitt@gmail.com>
2019-12-07 12:04:01 -0600

    WIP working on filtering by category

--- FlowersAndFloofs.ui/src/Components/Shop/Shop.js
+++ FlowersAndFloofs.ui/src/Components/Shop/Shop.js
@@ -2,34 +2,60 @@ import React from 'react';
 
 import bundleRequest from '../../DataRequests/bundleRequest';
 import SingleBundle from '../SingleBundle/SingleBundle';
+import occasionRequest from '../../DataRequests/occasionRequest';
 
+import { ButtonGroup, Button } from 'reactstrap';
 
 class Shop extends React.Component {
   state = {
-    bundles: []
+    bundles: [],
+    filteredBundles: [],
+    occasions: [],
+  }
+
+  getBundles = () => {
+    bundleRequest.getAllBundles()
+    .then(bundles => this.setState({ bundles, filteredBundles: bundles }))
+    .catch(err => console.error('no bundles for you', err))
+  }
+
+  filterBundlesByCategory = (e) => {
+    e.preventDefault();
+    const buttonCategory = e.target.id;
+    const regex = new RegExp(buttonCategory, 'gi');
+    const { bundles } = this.state;
+    this.setState({ filteredBundles: bundles });
+    const filteredResults = this.state.filteredBundles.filter(bundle => bundle.occasionId.match(regex));
+    this.setState({ filteredBundles: filteredResults });
+  }
+
+  showAllBundles = (e) => {
+    bundleRequest.getAllBundles()
+      .then(bundles => this.setState({ bundles, filteredBundles: bundles }))
+      .catch(err => console.error('unable to reset state', err));
   }
 
   componentDidMount(){
-    bundleRequest.getAllBundles().then(data => {
-      let allBundles = [...data];
-      this.setState({bundles: allBundles});
-    })
-    console.error(this.state.bundles);
-
+    this.getBundles();
+    occasionRequest.getAllOccasions().then(data => {
+      let allOccasions = [...data];
+      this.setState({occasions: allOccasions});
+    });
+    console.error(this.state.occasions);
   }
-
-  showAllBundles(){
-    const allBundles = [...this.state.bundles];
-    console.error(allBundles);
-
-    return allBundles.map(value => <div><h2>Bundle: </h2>{value.id}</div>);
-  }
-
-
 
   render() {
-
-    const makeBundles = this.state.bundles.map(bundle => (
+    const makeOccasions = this.state.occasions.map(occasion => (
+      <Button
+        key={occasion.id}
+        id={occasion.id}
+        className="occasionCategoryButton"
+        onClick={this.filterByCategory}
+      >
+        {occasion.name}
+      </Button>
+    ))
+    const makeBundles = this.state.filteredBundles.map(bundle => (
       <SingleBundle 
       key={bundle.id}
       flowerId={bundle.flowerId}
@@ -44,9 +70,16 @@ class Shop extends React.Component {
     ));
     return (
       <div className="container">
+        <h1>Categories</h1>
+        <div className = "row">
+          <ButtonGroup>
+            {makeOccasions}
+            <Button id="showAllBtn" onClick={this.showAllBundles}>Show All</Button>
+          </ButtonGroup>
+        </div>
         <h1>Shop</h1>
         <div className="row">
-        {makeBundles}
+            {makeBundles}
         {images}
         </div>
 
