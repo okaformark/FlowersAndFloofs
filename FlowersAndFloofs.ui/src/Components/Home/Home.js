f229bc710294c6dc93b8d58a250022868631be66
okaformark <okaformark@gmail.com>
2019-12-13 21:26:30 -0600

    added some addToCart/Quantity functionalities

--- FlowersAndFloofs.ui/src/Components/Home/Home.js
+++ FlowersAndFloofs.ui/src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
 import Shop from '../Shop/Shop';
+import { isNull } from 'util';
+import { isEmptyStatement } from '@babel/types';
 
 const cart = [];
 const cartForQuantity = [];
-const tempCart= [];
+let tempCart = {};
 class Home extends React.Component {
 
 state = {
@@ -13,43 +15,52 @@ state = {
 
 
     getCartLength = ()=> {
-        const cartLen = this.state.myCart.length;
+        const cartLen = this.state.myCart[0].length;
         return cartLen;
       }
 
+      addQuantityToCart = (cartWithQuantity) => { 
+            // cartForQuantity.push(cartWithQuantity);
+            //  tempCart = this.state.myCart;
+             tempCart = cartWithQuantity
+             console.error(tempCart, "ppppp")
+            //  this.setState({myCart: [...tempCart]}, () =>{
+            //      console.error("yay", this.state.myCart)
+            //   this.getCartLength()
+            //  }); 
+            return tempCart;
+        }
     
     handleAddToCart= (bundle) => {
-        const newCart = Object.assign({quantity:0}, bundle);
-        cart.push(newCart);
+        //const newCart = Object.assign({quantity:0}, bundle);
+        const newCart = this.addQuantityToCart(tempCart);
+        console.log(newCart,"kkkkkkkkk")
+        if(Object.keys(newCart).length ==0){
+            alert("enter quantity")
+        }
+        else{
+            cart.push(newCart);
+        console.error("sds", cart)
 
         // returns product already in the cart that matches the one the users clicks on 
-        const existingCart = cart.filter(product => product.id === bundle.id);
+        //const existingCart = cart.filter(product => product.id === bundle.id);
     
         
         // returns products already in the cart different from the one the user adds to cart
         const uniqueObjects = [...new Map(cart.map(item => [item.id, item])).values()]
 
         //if there are  matching products that exists
-         if(existingCart.length > 0 && uniqueObjects.length > 0){
+         if(cart.length > 0 && uniqueObjects.length > 0){
            this.setState({ myCart: [...uniqueObjects]}, () =>{
+               console.error(this.state.myCart);
            this.getCartLength()
     
            })
         }
+        }
        };  
 
-       addQuantityToCart = (cartWithQuantity) => { 
-           cartForQuantity.push(cartWithQuantity);
-           const { myCart } = this.state;
-           let i = 0;
-           cartForQuantity.forEach((product) => {
-             this.setState({myCart: [...cartForQuantity]}, () =>{
-                 console.error("yay", this.state.myCart)
-              this.getCartLength()
-             });   
-           })
-        }
-
+       
 
     render (){
         const { length } = this.state.myCart;
--- FlowersAndFloofs.ui/src/Components/SingleBundle/SingleBundle.js
+++ FlowersAndFloofs.ui/src/Components/SingleBundle/SingleBundle.js
@@ -58,33 +58,34 @@ class SingleBundle extends React.Component {
           <CardHeader>{flower.title} {"&"} {puppy.title}</CardHeader>
           <CardImg top width="100%" src={this.props.image} alt="Card image cap" />
           <CardText className="">{this.props.description}</CardText>
-            <CardText>Price: {this.calcBundlePrice()}</CardText>
+          <CardText>Price: {this.calcBundlePrice()}</CardText>
+            <select className="custom-select custom-select-sm" onChange={this.getQuantity}>
+            <option defaultValue={'Quantity'}>Quantity</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+            <option value="13">13</option>
+            <option value="14">14</option>
+            <option value="15">15</option>
+            <option value="16">16</option>
+            <option value="17">17</option>
+            <option value="18">18</option>
+            <option value="19">19</option>
+            <option value="20">20</option>
+          </select>
         </Card>
         <Button type="button" className="btn btn-danger btn-sm " onClick={this.props.handleAddToCart.bind(this, bundle)}>Add to Cart
         </Button>
-        <select className="custom-select custom-select-sm" onChange={this.getQuantity}>
-          <option defaultValue={'Quantity'}>Quantity</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-          <option value="11">11</option>
-          <option value="12">12</option>
-          <option value="13">13</option>
-          <option value="14">14</option>
-          <option value="15">15</option>
-          <option value="16">16</option>
-          <option value="17">17</option>
-          <option value="18">18</option>
-          <option value="19">19</option>
-          <option value="20">20</option>
-        </select>
+       
       </div>
     )
   }
