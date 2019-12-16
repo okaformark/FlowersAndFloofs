d3ff4fc30ecbffc3f37f170e85b84269f59b505c
okaformark <okaformark@gmail.com>
2019-12-16 13:11:01 -0600

    cart modal works on click and other functionalities

--- FlowersAndFloofs.ui/src/Components/NavBar/NavBar.js
+++ FlowersAndFloofs.ui/src/Components/NavBar/NavBar.js
@@ -25,36 +25,6 @@ class NavBar extends React.Component {
         });
     }
 
-    makeModalTable=()=>{
-        return(
-            <div>
-                <div className="row">
-                    <div className="col-lg-12 p-5 bg-white rounded shadow-sm mb-5">
-                        <div className="table-responsive">
-                            <table className="table">
-                                <thead>
-                                    <tr>
-                                        <th scope="col" className="border-0 bg-light">
-                                        <div className="p-2 px-3 text-uppercase">Product</div>
-                                        </th>
-                                        <th scope="col" className="border-0 bg-light">
-                                            <div className="py-2 text-uppercase">Total Price</div>
-                                        </th>
-                                        <th scope="col" className="border-0 bg-light">
-                                            <div className="py-2 text-uppercase">Quantity</div>
-                                        </th>
-                                        <th scope="col" className="border-0 bg-light">
-                                            <div className="py-2 text-uppercase">Remove</div>
-                                        </th>
-                                    </tr>
-                                </thead>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
     render() {
         const { cart } = this.props;
         const { price } = this.props;
@@ -129,7 +99,7 @@ class NavBar extends React.Component {
                 </nav>
                      <Modal isOpen={this.state.modalShow}
                             toggle={this.toggle} 
-                            className={this.props.className} 
+                            className="modal-dialog modal-lg"
                             modalTransition={{ timeout: 700 }} 
                             backdropTransition={{ timeout: 1300 }}>
                         <ModalHeader toggle={this.toggle}>Your Cart</ModalHeader>
