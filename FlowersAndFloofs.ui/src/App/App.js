d3b0b7176352f5f42dda01e09379287f8a26c200
Matt Gill <mtgill90@outlook.com>
2020-01-04 21:15:59 -0600

    wip

--- FlowersAndFloofs.ui/src/App/App.js
+++ FlowersAndFloofs.ui/src/App/App.js
@@ -40,21 +40,13 @@ const PrivateRoute = ({ component: Component, authed, ...rest }) => {
   return <Route {...rest} render={props => routeChecker(props)} />;
 };
 
-const cart = [];
-let tempCart = {};
-const tempUnitPrice = [];
-const tempPrice = [];
-
 class App extends React.Component {
   state = {
     authed: false,
-    customerObj: {},
-    myCart:[],
-    price:[],
-    unitPrice: [],
-    name: '',
-    }
-  
+    customerObj: {
+      name: '',
+    },
+  }
 
   componentDidMount() {
     this.removeListener = firebase.auth().onAuthStateChanged((user) => {
@@ -69,47 +61,6 @@ class App extends React.Component {
   componentWillUnmount() {
     this.removeListener();
   }
-
-  deleteItem = (id) =>{
-    this.setState({myCart: this.state.myCart.filter(item => item.id !== id)})
-  }
-
-  getCartLength = ()=> {
-      const cartLen = this.state.myCart.length;
-      return cartLen;
-    }
-
-    getPrice = (price,unitPrice) =>{
-      tempPrice.push(price);
-      tempUnitPrice.push(unitPrice);
-      this.setState({price:tempPrice, unitPrice:tempUnitPrice}, 
-      ()=>{console.error(this.state.unitPrice, this.state.price,"kkkkkk")})
-    } 
-
-    addQuantityToCart = (cartWithQuantity) => { 
-           tempCart = cartWithQuantity;
-          return tempCart;
-      }
-
-  handleAddToCart= () => {
-      const newCart = this.addQuantityToCart(tempCart);
-      if(Object.entries(newCart).length === 0){
-          alert("Enter Quantity");
-      }
-      else{
-          cart.push(newCart);
-      // returns products already in the cart different from the one the user adds to cart
-      const uniqueObjects = [...new Map(cart.map(item => [item.id, item])).values()]
-
-      //if there are  matching products that exists
-       if(cart.length > 0 && uniqueObjects.length > 0){
-         this.setState({ myCart: [...uniqueObjects]}, () =>{
-             console.error(this.state.myCart);
-         this.getCartLength();
-         })
-      }
-  }
-  };
 
   // getCustomer = () => {
   //   if (this.state.authed) {
