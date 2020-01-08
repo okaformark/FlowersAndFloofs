63efc0837e5942a46a63e0b9bbb54b8c3a6231b2
okaformark <okaformark@gmail.com>
2020-01-07 19:47:11 -0600

    wip

--- FlowersAndFloofs.ui/src/Components/Checkout/Checkout.js
+++ FlowersAndFloofs.ui/src/Components/Checkout/Checkout.js
@@ -3,8 +3,7 @@ import React from 'react';
 class Checkout extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Checkout Page!</h1>
+            <div className="container">
             </div>
         );
     }
