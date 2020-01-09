2614e68e056857f169bd3aca0928a5621b4051db
EmilyDeWitt <emilykdewitt@gmail.com>
2020-01-09 12:18:57 -0600

    Billing and shipping address WIP - not yet functioning but pushing up so Mark can start on credit card entry and posting orders

--- FlowersAndFloofs.ui/src/Components/Checkout/Checkout.js
+++ FlowersAndFloofs.ui/src/Components/Checkout/Checkout.js
@@ -13,65 +13,66 @@ import shippingAddressRequest from '../../DataRequests/shippingAddressRequest';
 import SingleAddress from '../SingleAddress/SingleAddress';
 
 class Checkout extends React.Component {
-    state = {
-        billingAddresses: [],
-        shippingAddresses: [],
-    }
+    // state = {
+    //     billingAddresses: [],
+    //     shippingAddresses: [],
+    // }
 
-    componentDidMount(){
-        billingAddressRequest.getUserBillingAddresses().then(data => {
-            this.setState({billingAddresses: data})
-            console.error("all billing addresses: ", this.state.billingAddresses);
-        });
-        shippingAddressRequest.getUserShippingAddresses().then(data => {
-            this.setState({shippingAddresses: data})
-            console.error("all shipping addresses: ", this.state.shippingAddresses);
-        });
-    }
+    // componentDidMount(){
+    //     billingAddressRequest.getUserBillingAddresses().then(data => {
+    //         this.setState({billingAddresses: data})
+    //         console.error("all billing addresses: ", this.state.billingAddresses);
+    //     });
+    //     shippingAddressRequest.getUserShippingAddresses().then(data => {
+    //         this.setState({shippingAddresses: data})
+    //         console.error("all shipping addresses: ", this.state.shippingAddresses);
+    //     });
+    // }
 
-    makeShippingAddresses = (results) => {
-        return results.map(address => (
-            <SingleAddress 
-                key={address.id}
-                streetAddress={address.streetAddress}
-                aptOrHouseNum={address.aptOrHouseNum}
-                city={address.city}
-                state={address.state}
-                zipCode={address.zipCode}
-            />
-        ));
-    }
+    // makeShippingAddresses = (results) => {
+    //     return results.map(address => (
+    //         <SingleAddress 
+    //             key={address.id}
+    //             streetAddress={address.streetAddress}
+    //             aptOrHouseNum={address.aptOrHouseNum}
+    //             city={address.city}
+    //             state={address.state}
+    //             zipCode={address.zipCode}
+    //         />
+    //     ));
+    // }
 
-    makeBillingAddresses = (results) => {
-        return results.map(address => (
-            <SingleAddress 
-                key={address.id}
-                streetAddress={address.streetAddress}
-                aptOrHouseNum={address.aptOrHouseNum}
-                city={address.city}
-                state={address.state}
-                zipCode={address.zipCode}
-            />
-        ));
-    }
+    // makeBillingAddresses = (results) => {
+    //     return results.map(address => (
+    //         <SingleAddress 
+    //             key={address.id}
+    //             streetAddress={address.streetAddress}
+    //             aptOrHouseNum={address.aptOrHouseNum}
+    //             city={address.city}
+    //             state={address.state}
+    //             zipCode={address.zipCode}
+    //         />
+    //     ));
+    // }
 
     render(){
-        const makeShippingAddressCards = (this.state.shippingAddresses.length > 0 ? this.makeShippingAddresses(this.state.shippingAddresses) :
-            console.error('no shipping addresses found'));
+        // const makeShippingAddressCards = (this.state.shippingAddresses.length > 0 ? this.makeShippingAddresses(this.state.shippingAddresses) :
+        //     console.error('no shipping addresses found'));
 
-        const makeBillingAddressCards = (this.state.billingAddresses.length > 0 ? this.makeBillingAddresses(this.state.billingAddresses) :
-            console.error('no billing addresses found'));
+        // const makeBillingAddressCards = (this.state.billingAddresses.length > 0 ? this.makeBillingAddresses(this.state.billingAddresses) :
+        //     console.error('no billing addresses found'));
 
         return (
             <React.Fragment>
-                <Row>
+                <p>This is where my shipping and billing address stuff will go.</p>
+                {/* <Row>
                     <h3>Shipping Addresses:</h3>
                     {makeShippingAddressCards}
                 </Row>
                 <Row>
                     <h3>Billing Addresses:</h3>
                     {makeBillingAddressCards}
-                </Row>
+                </Row> */}
             </React.Fragment>
         )
     }
